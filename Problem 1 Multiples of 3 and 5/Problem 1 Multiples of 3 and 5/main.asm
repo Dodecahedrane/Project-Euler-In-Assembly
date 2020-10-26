bbacb26a191b3043a6b6273acdfb1d4b89bd8d68
Oliver Guy <oliver.guy-1@students.plymouth.ac.uk>
2020-10-26 21:25:30 +0000

    Problem 1 Complete

--- Problem 1 Multiples of 3 and 5/Problem 1 Multiples of 3 and 5/main.asm
+++ Problem 1 Multiples of 3 and 5/Problem 1 Multiples of 3 and 5/main.asm
@@ -6,33 +6,36 @@ ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
 .data ; data segment
 	
-	arr DWORD ?
+	array DWORD 1000 DUP(0h)		;malloc 1000 bytes of the stack (or at least i think thats what this is doing lol)
 	rem DWORD ?
 	quo DWORD ?
 	i DWORD 1
-	cnt DWORD ?
 	max DWORD 1000
 	sum DWORD ?
+	arrayAdd DWORD ?
 
 .code ; code segment
 
 main PROC ; main procedure
 
-	mov ebx, 5
-	mov ecx, 3
+	mov esi, 0
 
 	testFiveThree:
 
 		;div by 5 then test, jump to quoZero if true
+		mov ebx, 5
 		mov eax, i
+		xor edx, edx
 		div ebx
 		mov quo, edx
 		cmp quo, 0
 		je quoZero
 
 		;div by 3 then test, jump to quoZero if true
+		mov ebx, 3
 		mov eax, i
-		div ecx
+		xor edx, edx
+		div ebx
 		mov quo, edx
 		mov ecx, 0
 		cmp ecx, quo
@@ -46,16 +49,37 @@ main PROC ; main procedure
 		inc i
 		mov edx, i
 		cmp edx, max
-		je testFiveThree
-		jmp done
+		je clearRegs
+		jmp testFiveThree
+		
 
 	quoZero:
 
 		;incrment cnt then append to array
-		inc cnt
-		lea ebx, arr
-		mov [ebx + TYPE arr * cnt], i
+		lea ebx, array
+		mov edx, i
+		mov [ebx + TYPE array * esi], edx
+		inc esi
 		jmp increment
+
+	clearRegs:
+		mov edx, esi
+		mov esi, 0
+		jmp sumAll
+
+	sumAll:
+
+		;sum all items in array
+		
+		mov ecx, sum
+		lea ebx, array
+		mov eax, [ebx + TYPE array * esi]
+		add eax, ecx
+		mov sum, eax
+		inc esi
+		cmp esi, max
+		jne sumAll
+		jmp done
 
 	done:
 	INVOKE ExitProcess, 0 ; call exit function
