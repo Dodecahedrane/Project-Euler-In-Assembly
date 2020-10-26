0b7751b7f14c2607d0441425273dbf126c554a7e
Oliver Guy <oliver.guy-1@students.plymouth.ac.uk>
2020-10-26 19:56:39 +0000

    Task 1 Build Errors

--- Problem 1 Multiples of 3 and 5/Problem 1 Multiples of 3 and 5/main.asm
+++ Problem 1 Multiples of 3 and 5/Problem 1 Multiples of 3 and 5/main.asm
@@ -5,13 +5,59 @@
 ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
 .data ; data segment
-	; define your variables here
+	
+	multiples DWORD ?
+	rem DWORD ?
+	quo DWORD ?
+	i DWORD 1
+	cnt DWORD ?
+	max DWORD 1001
+	sum DWORD ?
 
 .code ; code segment
 
 main PROC ; main procedure
-	; write your assembly code here
 
+	mov ebx, 5
+	mov ecx, 3
+
+	testFiveThree:
+
+		;div by 5 then test, jump to quoZero if true
+		mov eax, i
+		div ebx
+		mov quo, edx
+		cmp quo, 0
+		je quoZero
+
+		;div by 3 then test, jump to quoZero if true
+		mov eax, i
+		div ecx
+		mov quo, edx
+		mov ecx, 0
+		cmp ecx, quo
+		je quoZero
+	    
+		jmp increment
+
+	increment:
+
+		;increment i then jump to test if less than max or jump to done if not
+		inc i
+		mov edx, i
+		cmp edx, max
+		je testFiveThree
+		jmp done
+
+	quoZero:
+
+		;incrment cnt then append to array
+		inc cnt
+		lea ebx, multiples
+		mov [ebx + TYPE multiples * cnt], i
+		jmp increment
+
+	done:
 	INVOKE ExitProcess, 0 ; call exit function
   
 main ENDP ; exit main procedure
