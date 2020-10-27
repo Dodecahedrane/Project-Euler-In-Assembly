3fd5f53d67bc90b8a4aec126e11ff53b7ca411c7
Oliver Guy <oliver.guy-1@students.plymouth.ac.uk>
2020-10-27 14:37:24 +0000

    Problem 2 Progress

--- Problem 2 Even Fibonacci numbers/Problem 2 Even Fibonacci numbers/main.asm
+++ Problem 2 Even Fibonacci numbers/Problem 2 Even Fibonacci numbers/main.asm
@@ -5,13 +5,46 @@
 ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
 .data ; data segment
-	; define your variables here
+	
+	sum DWORD ?
+	current DWORD ?
+
 
 .code ; code segment
 
 main PROC ; main procedure
-	; write your assembly code here
+	mov eax, 1
+	mov ebx, 2
 
+	increment:
+		mov eax,
+		mov ebx,
+		add eax, ebx
+		mov current, eax
+		mov 
+		
+		;check if even
+		mov ecx, 2
+		div ecx
+		xor edx, edx
+		cmp edx, 0
+		je check
+		jmp increment
+		
+
+
+	check:
+		;check if more than 4 million
+		cmp edx, 4000000
+		;if it isnt then add
+		jne addToSum
+		;if it is jump to done
+		jmp done
+	
+	addToSum:
+		mov eax, 
+
+	done:
 	INVOKE ExitProcess, 0 ; call exit function
   
 main ENDP ; exit main procedure
