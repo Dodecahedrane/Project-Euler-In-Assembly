b2bfe857c6988be6a8b1d8c9bd9cdf8b9eb70d6f
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2020-12-28 22:27:11 +0000

    Update

--- Problem 3 Largest Prime Factor/main.asm
+++ Problem 3 Largest Prime Factor/main.asm
@@ -5,14 +5,47 @@
 ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
 .data ; data segment
-	; define your variables here
+	toFactor DWORD 600851475143
+    x DWORD ?
+    y DWORD ?
+    d DWORD ?
+    xMinusYAbs  DWORD ?
+    
 
 .code ; code segment
 
 main PROC ; main procedure
-	; write your assembly code here
+	
+    mainLoop:
+    
+    whileDEqua1:
+        ;x <- g(x)
+        ;y <- g(g(y))
+        ;d <- gcd(|x-y|, n)
+        ;if d = toFactor we fucked up
+        ;else return d as ans
+
+    
+    whileAEqualsB:
+        
+
+
+
 
 	INVOKE ExitProcess, 0 ; call exit function
   
 main ENDP ; exit main procedure
 END main  ; stop assembling
+
+
+
+;Do While a <> b
+;     Do While a > b
+;         c = a – b
+;         a = c
+;     Loop
+;     Do While b > a
+;         c = b – a
+;         b = c
+;     Loop
+; Loop
